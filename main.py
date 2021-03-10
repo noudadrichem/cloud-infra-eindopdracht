df4e2d22b05eee54ce88c6089764890cabdf1672
noud <noud@packstack.openstack.local>
2021-03-10 09:23:44 -0500

    session on login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, request, make_response, url_for, redirect, flash, render_template, session
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
 import authomatic
@@ -7,11 +7,16 @@ import logging
 from config import CONFIG
 
 # Instantiate Authomatic.
-authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
+authomatic = Authomatic(CONFIG, 'AnderG3h3impi3Hi3rzo!', report_errors=False)
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder='html')
+app.config['SECRET_KEY'] = 'Sup3rgeheimhee!'
+
 @app.route('/')
 def index():
+    print('-__session start')
+    print(session)
+    print('-__session end')
     return render_template('index.html')
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
@@ -19,18 +24,25 @@ def login(provider_name):
     # We need response object for the WerkzeugAdapter.
     response = make_response()
     # Log the user in, pass it the adapter and the provider name.
-    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
+    result = authomatic.login(
+        WerkzeugAdapter(request, response),
+        provider_name,
+        session=session,
+        session_saver=lambda: app.save_session(session, response)
+    )
 
-    # If there is no LoginResult object, the login procedure is still pending.
+    print(session)
+
     if result:
         if result.user:
-            # We need to update the user to get more info.
             result.user.update()
+            session['email'] = result.user.email
+            session['id'] = result.user.id
+            session['name'] = result.user.name
+            session['token'] = 'super geheime token hierzo jeetje zeg'
 
-        # The rest happens inside the template.
         return render_template('login.html', result=result)
 
-    # Don't forget to return the response.
     return response
 
 # Run the app on port 5000 on all interfaces, accepting only HTTPS connections
